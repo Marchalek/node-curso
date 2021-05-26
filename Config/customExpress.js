1b8cbb737702411c3f7219fdac395d18cf2657b6
Marchalek <marchalek@hotmail.com>
2021-05-26 15:49:07 -0300

    Comentado dos os comandos para saber oq cada coisa faz

--- Config/customExpress.js
+++ Config/customExpress.js
@@ -1,19 +1,19 @@
-const express = require ('express')
-const consign = require ('consign')
-const bodyParser = require ('body-parser')
+const express = require ('express')  //puxa o módulo express
+const consign = require ('consign') //puxa o módulo consign
+const bodyParser = require ('body-parser') //puxa o módulo bodyparses
 
-module.exports = () => {
+module.exports = () => { //exporrta oq vêm a seguir
 
-const app = express ()
+const app = express () //faz com q a api utilize o módulo express
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true})) //faz a api usar URLENCODED
+app.use(bodyParser.json())  //faz a api usar .json
 
 consign()
-    .include('controlers')
+    .include('controlers')  //coloca as rotas criadas em CONTROLERS dentro da api
     .into(app)
 
-    return app
+    return app //retorna a api com os controlers inseridos
 }
 
 
--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
-const mysql = require ('mysql')
+const mysql = require ('mysql') //puxa o móulo mysql para o programa
 
-const conexao = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'admin',
-    database: 'agenda-petshop',
+const conexao = mysql.createConnection({ //cria uma conexão de mysql
+    host: 'localhost', //quem vai ser o host da conexão
+    port: 3306,  //porta da conexão
+    user: 'root', //usaurio
+    password: 'admin',  //senha
+    database: 'agenda-petshop', //nome do baco de dados
 })
 
-module.exports = conexao
+module.exports = conexao //exporta essa conexao
